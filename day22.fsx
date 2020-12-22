24bd8b8c60ceb3e5d37358498af0f2b54428a055
Robert Franke <robert.franke@nirx.de>
2020-12-22 21:15:12 +0100

    Some cleanup

--- day22.fsx
+++ day22.fsx
@@ -43,17 +43,21 @@ let rec game2 (deck1: int list) (deck2: int list) history =
         | ([], x) -> (2, x)
         | (x1 :: x1s, x2 :: x2s) ->
 
-            if (x1s.Length >= x1) && (x2s.Length >= x2) then
-                let (winner, _) =
-                    game2 x1s.[..x1 - 1] x2s.[..x2 - 1] Set.empty
+            let winner =
+                if (x1s.Length >= x1) && (x2s.Length >= x2)
+                then fst (game2 (List.take x1 x1s) (List.take x2 x2s) Set.empty)
+                else if x1 > x2
+                then 1
+                else 2
 
-                if winner = 1
-                then game2 (x1s @ [ x1; x2 ]) x2s (Set.add (deck1, deck2) history)
-                else game2 x1s (x2s @ [ x2; x1 ]) (Set.add (deck1, deck2) history)
-            else if x1 > x2 then
-                game2 (x1s @ [ x1; x2 ]) x2s (Set.add (deck1, deck2) history)
+            if winner = 1
+
+            then
+                let deck1new = x1s @ [ x1; x2 ]
+                game2 deck1new x2s (Set.add (deck1, deck2) history)
             else
-                game2 x1s (x2s @ [ x2; x1 ]) (Set.add (deck1, deck2) history)
+                let deck2new = x2s @ [ x2; x1 ]
+                game2 x1s deck2new (Set.add (deck1, deck2) history)
 
 let add el (index, sum) = (index + 1, sum + index * el)
 let value deck = snd (List.foldBack add deck (1, 0))
