bcb439367302a7ba337cc6ecde79091ed0a13bef
Robert Franke <robert.franke@nirx.de>
2020-12-23 15:45:25 +0100

    More micro-optimizations for day 22

--- day22.fsx
+++ day22.fsx
@@ -1,4 +1,5 @@
 open System
+open System.Collections.Generic
 open System.IO
 
 let inputs =
@@ -39,9 +40,11 @@ type Hash =
         new(h1: int, h2: int) = { hash1 = h1; hash2 = h2 }
     end
 
-let rec game2 (deck1: int array) (deck2: int array) (history: ResizeArray<Hash>) =
+let rec game2 (deck1: int array) (deck2: int array) (history: HashSet<Hash>) =
 
-    if history.Contains(Hash(hash deck1, hash deck2)) then
+    let deckHash = Hash(hash deck1, hash deck2)
+
+    if not (history.Add(deckHash)) then
         (1, deck1)
     else
 
@@ -56,24 +59,29 @@ let rec game2 (deck1: int array) (deck2: int array) (history: ResizeArray<Hash>)
         let winner =
             if (Array.length deck1 > x1)
                && (Array.length deck2 > x2) then
-                fst (game2 (Array.sub deck1 1 x1) (Array.sub deck2 1 x2) (ResizeArray<Hash>()))
+                let x1s = Array.sub deck1 1 x1
+                let x2s = Array.sub deck2 1 x2
+                fst (game2 x1s x2s (HashSet<Hash>()))
             else if x1 > x2 then
                 1
             else
                 2
 
-        let x1s = Array.tail deck1
-        let x2s = Array.tail deck2
-
-        history.Add(Hash(hash deck1, hash deck2))
-
         if winner = 1
 
         then
-            let deck1new = Array.append x1s [| x1; x2 |]
+            let x2s = Array.tail deck2
+            let deck1new = Array.create (deck1.Length + 1) 0
+            Array.blit deck1 1 deck1new 0 (deck1.Length - 1)
+            deck1new.[deck1.Length - 1] <- x1
+            deck1new.[deck1.Length] <- x2
             game2 deck1new x2s history
         else
-            let deck2new = Array.append x2s [| x2; x1 |]
+            let x1s = Array.tail deck1
+            let deck2new = Array.create (deck2.Length + 1) 0
+            Array.blit deck2 1 deck2new 0 (deck2.Length - 1)
+            deck2new.[deck2.Length - 1] <- x2
+            deck2new.[deck2.Length] <- x1
             game2 x1s deck2new history
 
 let add el (index, sum) = (index + 1, sum + index * el)
@@ -83,7 +91,7 @@ let value deck = snd (List.foldBack add deck (1, 0))
 let (_, windeck1) = game1 deck1 deck2 Set.empty
 
 let (_, windeck2) =
-    game2 (deck1 |> Array.ofList) (deck2 |> Array.ofList) (ResizeArray<Hash>())
+    game2 (deck1 |> Array.ofList) (deck2 |> Array.ofList) (HashSet<Hash>())
 
 printfn "Part 1: %d" (value windeck1)
 printfn "Part 2: %d" (value (windeck2 |> List.ofArray))
