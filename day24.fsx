157b9f448f3d9f81c879dc3d62cb9ef8962b8dc7
Robert Franke <robert.franke@nirx.de>
2020-12-28 15:36:48 +0100

    Performance optimization for day 24

--- day24.fsx
+++ day24.fsx
@@ -1,4 +1,5 @@
 open System.IO
+open System.Collections.Generic
 open System.Text.RegularExpressions
 
 let regex = Regex("(nw|ne|se|sw|e|w)")
@@ -11,25 +12,86 @@ let isEven n = (n % 2 = 0)
 let isOdd n = not (isEven n)
 
 let move directions =
-    let start = (0, 0)
+    let start = struct (0, 0)
 
     let step pos dir =
-        let (c, r) = pos
+        let struct (c, r) = pos
 
         match dir with
-        | "e" -> (c + 1, r)
-        | "w" -> (c - 1, r)
-        | "se" -> if isEven r then (c + 1, r + 1) else (c, r + 1)
-        | "ne" -> if isEven r then (c + 1, r - 1) else (c, r - 1)
-        | "sw" -> if isOdd r then (c - 1, r + 1) else (c, r + 1)
-        | "nw" -> if isOdd r then (c - 1, r - 1) else (c, r - 1)
+        | "e" -> struct (c + 1, r)
+        | "w" -> struct (c - 1, r)
+        | "se" -> if isEven r then struct (c + 1, r + 1) else struct (c, r + 1)
+        | "ne" -> if isEven r then struct (c + 1, r - 1) else struct (c, r - 1)
+        | "sw" -> if isOdd r then struct (c - 1, r + 1) else struct (c, r + 1)
+        | "nw" -> if isOdd r then struct (c - 1, r - 1) else struct (c, r - 1)
         | e -> failwith $"Unknown direction {e}"
 
-    let (c, r) = directions |> Seq.fold step start
-    (c, r)
+    let struct (c, r) = directions |> Seq.fold step start
+    struct (c, r)
 
 let updateMap (blackTiles: Set<'a>) (field: 'a) =
     if Set.contains field blackTiles then Set.remove field blackTiles else Set.add field blackTiles
+
+
+type TileState =
+    | Black
+    | White
+
+let neighbours tile =
+    let struct (c, r) = tile
+
+    if isEven r then
+        [| struct (c + 1, r)
+           struct (c - 1, r)
+           struct (c + 1, r + 1)
+           struct (c + 1, r - 1)
+           struct (c, r + 1)
+           struct (c, r - 1) |]
+    else
+        [| struct (c + 1, r)
+           struct (c - 1, r)
+           struct (c, r + 1)
+           struct (c, r - 1)
+           struct (c - 1, r + 1)
+           struct (c - 1, r - 1) |]
+
+
+let calcNewState oldState blackNeighbours =
+    match oldState with
+    | Black when blackNeighbours = 0 || blackNeighbours > 2 -> White
+    | White when blackNeighbours = 2 -> Black
+    | oldState -> oldState
+
+let countActiveNeighbours (blackTiles: HashSet<'a>) (coords: 'a) (neighbours: 'a -> array<'a>) =
+    coords
+    |> neighbours
+    |> Array.filter blackTiles.Contains
+    |> Array.length
+
+let cycle (neighbours: 'a -> array<'a>) (blackTiles: HashSet<'a>) =
+    let candidates =
+        blackTiles |> Seq.collect neighbours |> HashSet
+
+    seq {
+        for candidate in candidates do
+            let oldState =
+                if blackTiles.Contains(candidate) then Black else White
+
+            let activeNeighbours =
+                countActiveNeighbours blackTiles candidate neighbours
+
+            let newState = calcNewState oldState activeNeighbours
+            if newState = Black then candidate
+    }
+    |> HashSet
+
+let repeat (count: int) (f: 'T -> 'T) (initial: 'T) =
+    let rec helper i acc =
+        if i < count then helper (i + 1) (f acc) else acc
+
+    helper 0 initial
+
+#time
 
 let blackTiles =
     File.ReadAllLines("day24.txt")
@@ -38,71 +100,7 @@ let blackTiles =
 
 printfn "Part 1: %d" (blackTiles.Count)
 
-type TileState =
-    | Black
-    | White
-
-let neighbours tile =
-    let c, r = tile
-
-    if isEven r then
-        seq {
-            (c + 1, r)
-            (c - 1, r)
-            (c + 1, r + 1)
-            (c + 1, r - 1)
-            (c, r + 1)
-            (c, r - 1)
-        }
-    else
-        seq {
-            (c + 1, r)
-            (c - 1, r)
-            (c, r + 1)
-            (c, r - 1)
-            (c - 1, r + 1)
-            (c - 1, r - 1)
-        }
-
-
-let calcNewState oldState blackNeighbours =
-    match oldState with
-    | Black when blackNeighbours = 0 || blackNeighbours > 2 -> White
-    | White when blackNeighbours = 2 -> Black
-    | Black -> Black
-    | White -> White
-
-let countActiveNeighbours (blackTiles: Set<'a>) (coords: 'a) (neighbours: 'a -> seq<'a>) =
-    coords
-    |> neighbours
-    |> Seq.filter blackTiles.Contains
-    |> Seq.length
-
-let cycle (neighbours: 'a -> seq<'a>) (blackTiles: list<'a>) =
-    let candidates =
-        blackTiles
-        |> Seq.collect neighbours
-        |> Seq.distinct
-
-    let blackTilesSet = Set.ofList blackTiles
-
-    [ for candidate in candidates do
-        let oldState =
-            if blackTilesSet.Contains(candidate) then Black else White
-
-        let activeNeighbours =
-            countActiveNeighbours blackTilesSet candidate neighbours
-
-        let newState = calcNewState oldState activeNeighbours
-        if newState = Black then candidate ]
-
-let repeat (count: int) (f: 'T -> 'T) (initial: 'T) =
-    let rec helper i acc =
-        if i < count then helper (i + 1) (f acc) else acc
-
-    helper 0 initial
-
 let part2 =
-    repeat 100 (fun blackTiles -> cycle neighbours blackTiles) (List.ofSeq blackTiles)
+    repeat 100 (fun blackTiles -> cycle neighbours blackTiles) (blackTiles |> HashSet)
 
-printfn "Part 2: %d" (List.length part2)
+printfn "Part 2: %d" (Seq.length part2)
